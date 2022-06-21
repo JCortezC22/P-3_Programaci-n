0b77bef37128cc4d779afbaca323a360919e4e81
Jordy <Jordycortez112@gmail.com>
2022-06-20 18:04:22 -0600

    APIRestaurante

---
+++ API_RESTAURANTE/Restaurant/Restaurant/Controllers/ProductosController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Http;
+using System.Net.Http;
+
+namespace Restaurant.Controllers
+{
+    public class ProductosController : ApiController
+    {
+        [System.Web.Http.HttpPost]
+        public IHttpActionResult Add()
+        {
+            using (Models.RestaurantEntities db = new Models.RestaurantEntities())
+            {
+                var oProducto = new Models.Producto();
+                oProducto.Nombre = "Hamburguesa";
+                oProducto.Descripcion = "Botella";
+                oProducto.Precio = Convert.ToDecimal(2.50);
+                oProducto.Cantidad = 12;
+                oProducto.Estado = 1;
+                oProducto.Foto = "https://assets.unileversolutions.com/recipes-v2/218401.jpg";
+
+                db.Producto.Add(oProducto);
+                db.SaveChanges();
+            }
+            return Ok("¡Hecho! El insert del producto se hizo correctamente");
+
+        }
+
+    }
+}
